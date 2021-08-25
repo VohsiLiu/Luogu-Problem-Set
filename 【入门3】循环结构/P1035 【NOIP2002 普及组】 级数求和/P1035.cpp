9a21afbefbbc9244f565e09a901142b54b67841e
VohsiLiu <alan.turing.von.neumann@gmail.com>
2021-08-25 23:08:18 +0800

    2.9

--- 【入门3】循环结构/P1035 【NOIP2002 普及组】 级数求和/P1035.cpp
+++ 【入门3】循环结构/P1035 【NOIP2002 普及组】 级数求和/P1035.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){
+	int k=0;
+	cin >> k;
+	double sum=0.0;
+	long long int n =0;
+	for (long long int i =1;;i++){
+		sum +=1.0/i;
+		n++;
+		if (sum>k){
+			break;
 
+		}
+	}
+	cout << n;
 	return 0;
 }
