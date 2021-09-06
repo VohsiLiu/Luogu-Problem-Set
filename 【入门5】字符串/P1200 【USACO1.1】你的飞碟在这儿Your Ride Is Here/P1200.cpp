a4308d6a52a836899576ce6666c62c623eb89efd
VohsiLiu <alan.turing.von.neumann@gmail.com>
2021-09-06 22:47:06 +0800

    4.7

--- 【入门5】字符串/P1200 【USACO1.1】你的飞碟在这儿Your Ride Is Here/P1200.cpp
+++ 【入门5】字符串/P1200 【USACO1.1】你的飞碟在这儿Your Ride Is Here/P1200.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main(){
-
+	string a,b;
+	cin >> a >> b;
+	long long int an=1,bn=1;
+	for(int i=0;i<a.length();i++){
+		an*=int(a[i]-'A'+1);
+	}	
+	for(int i=0;i<b.length();i++){
+		bn*=int(b[i]-'A'+1);
+	}
+	if(an%47==bn%47){
+		cout << "GO";
+	}else{
+		cout << "STAY";
+	}
 	return 0;
 }
--- 【入门5】字符串/P1553 数字反转（升级版）/P1553.cpp
+++ 【入门5】字符串/P1553 数字反转（升级版）/P1553.cpp
@@ -16,8 +16,67 @@ string del_zero(string s){
 	return s;
 }
 
+string re_int(string s){
+	string x=s;
+	for(int i=0;i<s.length();i++){
+		x[i]=s[s.length()-i-1];
+	}
+	return del_zero(x);
+}
+
+string re_decimal(string s,int local){
+	string x1=s.substr(0,local);
+	string x2=s.substr(local+1,s.length());
+	if(x2=="0"){
+		return del_zero(re_int(x1))+".0";
+	}
+	return del_zero(re_int(x1))+"."+re_int(del_zero(x2));
+}
+
+string re_frac(string s,int local){
+	string x1=s.substr(0,local);
+	string x2=s.substr(local+1,s.length());
+	return del_zero(re_int(x1))+"/"+del_zero(re_int(x2));	
+}
+
+string re_percent(string s){
+	string x=s.substr(0,s.length()-1);
+	return del_zero(re_int(x))+"%";
+}
+
+
 int main(){
 	string s;
 	cin >> s;
+	int x=0,local=0;;
+	for(int i=0;i<s.length();i++){
+		if(s[i]=='.'){
+			x=1;
+			local=i;
+			break;
+		}
+		if(s[i]=='/'){
+			x=2;
+			local=i;
+			break;
+		}
+		if(s[i]=='%'){
+			x=3;
+			local=i;
+			break;
+		}
+	}
+	if(x==0){
+		cout << re_int(s);
+	}
+	if(x==1){
+		cout << re_decimal(s,local);
+	}
+	if(x==2){
+		cout << re_frac(s,local);
+	}
+	if(x==3){
+		cout << re_percent(s);
+	}
 	return 0;
 }
