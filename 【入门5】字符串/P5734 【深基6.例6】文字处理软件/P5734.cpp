f7398ada9faf01584d91b3fe471c82f0e18f34a1
VohsiLiu <alan.turing.von.neumann@gmail.com>
2021-09-08 09:02:46 +0800

    入门5

--- 【入门5】字符串/P5734 【深基6.例6】文字处理软件/P5734.cpp
+++ 【入门5】字符串/P5734 【深基6.例6】文字处理软件/P5734.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
 int main(){
-
+	int n;
+	string s;
+	cin >> n >> s;
+	for(int i=0;i<n;i++){
+		int t;
+		cin >> t;
+		if(t==1){
+			string x;
+			cin >> x;
+			s=s+x;
+			cout << s << endl;
+		}
+		if(t==2){
+			int begin;
+			int len;
+			cin >> begin >> len;
+			s=s.substr(begin,len);
+			cout << s << endl;
+		}
+		if(t==3){
+			int a;
+			string inse;
+			cin >> a >> inse;
+			string temp=s.substr(0,a);
+			string temp2=s.substr(a,s.length()-a);
+			s=temp+inse+temp2;
+			cout << s << endl;
+		}
+		if(t==4){
+			string fi;
+			cin >> fi;
+			if(s.find(fi)!=s.npos){
+				cout << s.find(fi) << endl;
+			}else{
+				cout << -1 << endl;
+			}
+		}
+	}
 	return 0;
 }
